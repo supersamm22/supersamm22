e599ebdc076a6398c8537611070a16df0468710e
antecuic <antecuic99@gmail.com>
2021-06-30 21:02:08 +0200

    database connected to server

---
+++ backend/models/User.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+// Create Schema
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  register_date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model('User', UserSchema);
